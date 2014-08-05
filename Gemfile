10b4aad17895d9601a465f14b298eac57eed85be
Duy Le <masaruduy@gmail.com>
2014-08-05 17:35:28 -0400

    creation done but still issues with account name addition

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ end
 group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
+  
 end
 
 # Use SCSS for stylesheets
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
 
   # GET /users/1
   # GET /users/1.json
-  def show
+ def show
+    @user = User.find(params[:id])
   end
-
   # GET /users/new
   def new
     @user = User.new
@@ -25,14 +25,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Expertise Online!"
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
 
+ 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
@@ -65,7 +65,11 @@ class UsersController < ApplicationController
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
-  def user_params
-    params.require(:user).permit(:name, :account, :email, :password_digest)
-  end
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
